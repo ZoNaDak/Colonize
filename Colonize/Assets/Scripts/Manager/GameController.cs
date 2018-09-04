fd7294b85f24dce12dc11a56e8c93501f5cffc89
ZoNaDak <kbh3719net@gmail.com>
2018-09-04 20:34:48 +0900

    add gamecontroller & CommunicateController & add Assets : Photon

---
+++ Colonize/Assets/Scripts/Manager/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager {
+	public class GameController : Singleton.MonoSingleton<GameController> {
+		[SerializeField] private Communicate.CommunicateManager communicator;
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+}
+
